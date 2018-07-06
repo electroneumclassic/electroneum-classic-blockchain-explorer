5ec6f8a8f5ebc028b67a02ba4f695de674c2b6fa
vans163 <vans_163@yahoo.com>
2018-07-06 00:05:49 +0000

    base blockexplorer

---
+++ cmake/FindElectroneum.cmake
@@ -0,0 +1,75 @@
+#------------------------------------------------------------------------------
+# CMake helper for the majority of the cpp-ethereum modules.
+#
+# This module defines
+#     Electroneum_XXX_LIBRARIES, the libraries needed to use ethereum.
+#     Electroneum_FOUND, If false, do not try to use ethereum.
+#
+# File addetped from cpp-ethereum
+#
+# The documentation for cpp-ethereum is hosted at http://cpp-ethereum.org
+#
+# ------------------------------------------------------------------------------
+# This file is part of cpp-ethereum.
+#
+# cpp-ethereum is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cpp-ethereum is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>
+#
+# (c) 2014-2016 cpp-ethereum contributors.
+#------------------------------------------------------------------------------
+
+set(LIBS common;blocks;cryptonote_basic;cryptonote_core;
+		cryptonote_protocol;daemonizer;mnemonics;epee;lmdb;
+		blockchain_db;ringct;wallet;cncrypto)
+
+set(Etn_INCLUDE_DIRS "${CPP_ELECTRONEUM_DIR}")
+
+# if the project is a subset of main cpp-ethereum project
+# use same pattern for variables as Boost uses
+
+foreach (l ${LIBS})
+
+	string(TOUPPER ${l} L)
+
+	find_library(Etn_${L}_LIBRARY
+		NAMES ${l}
+		PATHS ${CMAKE_LIBRARY_PATH}
+		PATH_SUFFIXES "/src/${l}" "/external/db_drivers/lib${l}" "/lib" "/src/crypto" "/contrib/epee/src"
+		NO_DEFAULT_PATH
+	)
+
+	set(Etn_${L}_LIBRARIES ${Etn_${L}_LIBRARY})
+
+	message(STATUS FindElectroneum " Etn_${L}_LIBRARIES ${Etn_${L}_LIBRARY}")
+
+	add_library(${l} STATIC IMPORTED)
+	set_property(TARGET ${l} PROPERTY IMPORTED_LOCATION ${Etn_${L}_LIBRARIES})
+
+endforeach()
+
+if (EXISTS ${ELECTRONEUM_BUILD_DIR}/external/easylogging++/libeasylogging.a)
+	add_library(easylogging STATIC IMPORTED)
+	set_property(TARGET easylogging
+			PROPERTY IMPORTED_LOCATION ${ELECTRONEUM_BUILD_DIR}/external/easylogging++/libeasylogging.a)
+endif()
+
+message(STATUS ${ELECTRONEUM_SOURCE_DIR}/build)
+
+# include electroneum headers
+include_directories(
+		${ELECTRONEUM_SOURCE_DIR}/src
+		${ELECTRONEUM_SOURCE_DIR}/external
+		${ELECTRONEUM_SOURCE_DIR}/build
+		${ELECTRONEUM_SOURCE_DIR}/external/easylogging++
+		${ELECTRONEUM_SOURCE_DIR}/contrib/epee/include
+		${ELECTRONEUM_SOURCE_DIR}/external/db_drivers/liblmdb)
